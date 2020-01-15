0aa98ff167ec65bbc9ffb2cb5d1cd740c1ef5574
YXX778526573 <jswxbhjn@163.com>
2020-01-15 14:22:24 +0800

    Create test_Fizz.py

---
+++ test_Fizz.py
@@ -0,0 +1,14 @@
+import unittest
+from Fizz import FizzBuzz
+
+class test(unittest.TestCase):
+    def test_fizz(self):
+        self.assertEqual("FizzBuzz",FizzBuzz(30))
+        self.assertEqual("Fizz",FizzBuzz(3))
+        self.assertEqual("Buzz",FizzBuzz(10))
+        self.assertEqual(31,FizzBuzz(31))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
