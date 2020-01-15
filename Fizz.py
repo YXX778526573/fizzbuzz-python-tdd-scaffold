8fbebd2d3fd04747414c444ff908d74c4b3bd2ba
YXX778526573 <jswxbhjn@163.com>
2020-01-15 14:50:44 +0800

    Update Fizz.py

--- Fizz.py
+++ Fizz.py
@@ -1,11 +1,11 @@
 def FizzBuzz(i):
-    if i%15==0:
+    if i%15 == 0:
         return "FizzBuzz"
 
-    if i%3==0:
+    if i%3 == 0:
         return "Fizz"
 
-    if i%5==0:
+    if i%5 == 0:
         return "Buzz"
 
     return i
